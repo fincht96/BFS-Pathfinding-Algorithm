e10dde0803183fec66250a339d0f5e9ac6f857b7
Thomas Finch <33233510+fincht96@users.noreply.github.com>
2018-11-28 11:31:18 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include <vector>
 #include <queue>
 
-const int grid_width = 4;
-const int grid_height = 4;
-
+const int grid_width = 5;
+const int grid_height = 5;
+const uint8_t start_pos = 20;
+const uint8_t goal_pos = 1;
 
 
 
@@ -13,6 +14,7 @@ const int grid_height = 4;
 template <typename G>
 void print(G grid, unsigned width, unsigned height)
 {
+	std::cout << std::endl << std::endl;
 	for (unsigned y = 0; y < height; y++)
 	{
 		for (unsigned x = 0; x < width; x++)
@@ -27,13 +29,14 @@ void print(G grid, unsigned width, unsigned height)
 
 
 template <typename G, typename T, typename S>
-void searchNeighbours(G& grid, T& trail, S& search_queue, int current_node)
+void searchNeighbours(G& grid, T& trail, S& search_queue, const int current_node)
 {
 	int node;
 
 
 	// search 'up' (node above current)
 	node = current_node - grid_width;
+
 
 	if ((node >= 0) && (trail[node] == -1) && (grid[node] != '@'))
 	{
@@ -55,7 +58,7 @@ void searchNeighbours(G& grid, T& trail, S& search_queue, int current_node)
 	// search 'down' (node below current)
 	node = current_node + grid_width;
 
-	if ((node < 16) && (trail[node] == -1) && (grid[node] != '@'))
+	if ((node < grid_width*grid_height) && (trail[node] == -1) && (grid[node] != '@'))
 	{
 		search_queue.push(node);		// add to search_queue
 		trail[node] = current_node;		// sets searched from to current node
@@ -65,7 +68,7 @@ void searchNeighbours(G& grid, T& trail, S& search_queue, int current_node)
 	// search 'left' (node to left of current)
 	node = current_node - 1;
 
-	if (((current_node % grid_width) > 0) && (trail[node] == -1) && (grid[node] != '@'))
+	if ((((node+1) % grid_width) != 0) && (trail[node] == -1) && (grid[node] != '@'))
 	{
 		search_queue.push(node);		// add to search_queue
 		trail[node] = current_node;	// sets searched from to current node
@@ -75,52 +78,44 @@ void searchNeighbours(G& grid, T& trail, S& search_queue, int current_node)
 
 // args: grid, start position, goal position
 template <typename G>
-void findShortestPath(G& grid, unsigned start, unsigned goal)	
+auto findShortestPath(G& grid, unsigned start, unsigned goal)
 {
 	// used to indicate which nodes have already been searched, and where they have been searched from
 	// a node with a 'trail' value of -1 has not been searched
 	// a node with 'trail' value of 8 has been searched by node 8
 	std::vector<int> trail(grid.size(), -1);		 // (one element for each node in grid)
-	
-	// used to hold a queue of nodes to be searched next
-	std::queue<int> search_queue;
 
-	search_queue.push(0);	// pushes the start node into queue
-	trail[0] = 0;			// records starting point in trail
+													 
+	std::queue<int> search_queue;					// used to hold a queue of nodes to be searched next
 
-	// if queue is empty then a path was not found
+	search_queue.push(start);		// pushes the start node into queue
+	trail[start] = start;			// records starting point in trail
+
+							// if queue is empty then a path was not found
 	while (!search_queue.empty())
 	{
 		int current_node = search_queue.front();		// fetch first element from queue
-		
+
 		if (current_node == goal)
 		{
 			std::vector<int> path;
-			
+
 			int node = goal;
 			path.insert(path.begin(), node);
 
-			node = trail[goal];
-			path.insert(path.begin(), node);
-			
+
 			while (node != start)
 			{
 				// read data at pointed node
 				node = trail[path[0]];
-			
+
 				// push data to front of path
 				path.insert(path.begin(), node);
 
+
 			}
 
-
-			std::cout << "\nThe shortest path from A to B (including start and goal) is as follows:\n";
-
-			for (const auto element : path)
-				std::cout << element << std::endl;
-
-				
-			return;
+			return path;
 		}
 
 
@@ -131,13 +126,73 @@ void findShortestPath(G& grid, unsigned start, unsigned goal)
 	}
 
 	std::cout << "No suitable path found.\n";
-	return;
+	std::cin.get();
+	//return;
 }
 
+// args: grid, start position, goal position
+template <typename G>
+bool plotShortestPath(G& grid, unsigned start, unsigned goal)
+{
+	// used to indicate which nodes have already been searched, and where they have been searched from
+	// a node with a 'trail' value of -1 has not been searched
+	// a node with 'trail' value of 8 has been searched by node 8
+	std::vector<int> trail(grid.size(), -1);		 // (one element for each node in grid)
 
+													 // used to hold a queue of nodes to be searched next
+	std::queue<int> search_queue;
+
+	search_queue.push(0);	// pushes the start node into queue
+	trail[0] = 0;			// records starting point in trail
+
+							// if queue is empty then a path was not found
+	while (!search_queue.empty())
+	{
+		int current_node = search_queue.front();		// fetch first element from queue
+
+		if (current_node == goal)
+		{
+			std::vector<int> path;
+
+			int node = goal;
+			path.insert(path.begin(), node);
+
+
+			while (node != start)
+			{
+				// read data at pointed node
+				node = trail[path[0]];
+
+				// push data to front of path
+				path.insert(path.begin(), node);
+
+
+			}
+
+
+			for (unsigned i = 0; i < path.size(); i++)
+			{
+				if ((grid[path[i]] != 'A') && (grid[path[i]] != 'B'))
+					grid[path[i]] = '-';
+			}
+
+			return true;
+		}
+
+
+		// search each of it's neighbours, adding their id if their 'trail' value is -1 and not restricted (i.e. on grid && !'@')
+		searchNeighbours(grid, trail, search_queue, current_node);
+
+		search_queue.pop();										// remove first element from queue
+	}
+
+	return false;
+}
 
 int main()
 {
+
+
 
 	/*
 	*	4*4 grid, where each element is labelled as follows:
@@ -146,28 +201,51 @@ int main()
 	*		4,  5,  6,  7
 	*		8,  9,  10, 11
 	*		12, 13, 14, 15
-	*	
+	*
 	*/
 	std::array<char, grid_width*grid_height> grid;
 
 
 	grid.fill('.');
 
-	grid[0] = 'A';
-	grid[8] = 'B';
+	grid[start_pos] = 'A';
+	grid[goal_pos] = 'B';
 
-	grid[3] = '@';
-	grid[4] = '@';
-	grid[5] = '@';
+	grid[0] = '@';
+	grid[6] = '@';
 	grid[7] = '@';
-	grid[9] = '@';
+	grid[8] = '@';
+	grid[13] = '@';
+	grid[16] = '@';
+	grid[18] = '@';
+	grid[21] = '@';
 
+	// prints the starting grid
 	print(grid, grid_width, grid_height);
 
-	findShortestPath(grid, 0, 8);
+	// returns the shortest path, if a valid path can be found
+	auto path = findShortestPath(grid, start_pos, goal_pos);
+
+
+	std::cout << "\nThe shortest path from A to B (including start and goal) is as follows:\n";
+
+	for (const auto element : path)
+		std::cout << element << std::endl;
+
+	// draws the shortest path on the maze
+	for (unsigned i = 0; i < path.size(); i++)
+	{
+
+
+		if ((grid[path[i]] != 'A') && (grid[path[i]] != 'B'))
+			grid[path[i]] = '*';
+	}
+
+
+	// prints the grid with path drawn
+	print(grid, grid_width, grid_height);
 
 	std::cin.get();
 
 	return 0;
 }
-
